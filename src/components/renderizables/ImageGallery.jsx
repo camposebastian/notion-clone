6ff1b02f98fbfc8060417c9bf4d876001d2df4a6
sebas-camp <sebastian.campos@scientone.com>
2024-07-29 12:28:54 -0500

    Update ImageGallery.jsx

--- src/components/renderizables/ImageGallery.jsx
+++ src/components/renderizables/ImageGallery.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+'use client'
 
-const ImageGallery = () => (
-  <>
-    <div className="grid grid-cols-3 gap-4">
+import React, { useState } from "react";
+
+function ImageGallery() {
+  const [images, setImages] = useState([]);
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+    const newImages = [];
+    for (let i = 0; i < files.length; i++) {
+      newImages.push({
+        file: files[i],
+        url: URL.createObjectURL(files[i]),
+      });
+    }
+    console.log(newImages)
+    setImages(newImages);
+  };
+
+  return(
+    <>
+    {/* <div className="grid grid-cols-3 gap-4">
       <div className="flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
         <div className="text-center">
           <p className="mx-auto h-12 w-12 text-gray-300">icon</p>
@@ -72,8 +90,44 @@ const ImageGallery = () => (
           </p>
         </div>
       </div>
-    </div>
-  </>
-);
+    </div> */}
+    <div className="grid grid-cols-3 gap-4 mt-4">
+        <div className="flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+          <div className="text-center">
+            <p className="mx-auto h-12 w-12 text-gray-300">icon</p>
+            <div className="mt-4 flex text-sm leading-6 text-gray-600">
+              <label
+                htmlFor="file-upload"
+                className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+              >
+                <span>Upload a file</span>
+                <input
+                  id="file-upload"
+                  name="file-upload"
+                  type="file"
+                  className="sr-only"
+                  multiple
+                  onChange={handleFileChange}
+                />
+              </label>
+              <p className="pl-1">or drag and drop</p>
+            </div>
+            <p className="text-xs leading-5 text-gray-600">
+              PNG, JPG, GIF up to 10MB
+            </p>
+          </div>
+        </div>
+        {images.map((image, index) => (
+          <div
+            key={index}
+            className="flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10"
+          >
+            <img src={image.url} alt={`Preview ${index}`} className="max-h-40" />
+          </div>
+        ))}
+      </div>
+    </>
+  )
+}
 
 export default ImageGallery;
