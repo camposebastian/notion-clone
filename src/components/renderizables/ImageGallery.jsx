7a46584e86fb0b4c58b0a100eec951fb027f75b3
sebas-camp <sebastian.campos@scientone.com>
2024-08-07 15:34:38 -0500

    Update ImageGallery.jsx

--- src/components/renderizables/ImageGallery.jsx
+++ src/components/renderizables/ImageGallery.jsx
@@ -1,28 +1,58 @@
 "use client";
 
+import { Description } from "@headlessui/react";
 import React, { useState } from "react";
 import { FaCloudUploadAlt } from "react-icons/fa";
+import { MdDeleteForever } from "react-icons/md";
+import { FaPen } from "react-icons/fa";
 
 function ImageGallery() {
   const [images, setImages] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const togglePopup = () => setIsOpen(!isOpen);
 
   const handleFileChange = (event) => {
     const files = event.target.files;
-    const newImages = [];
+    const newImages = [...images];
     for (let i = 0; i < files.length; i++) {
       newImages.push({
         file: files[i],
         url: URL.createObjectURL(files[i]),
+        description: "",
       });
     }
-    console.log(newImages);
     setImages(newImages);
   };
 
   const handleEditPictures = () => {
-    console.log("popup")
+    setIsOpen(!isOpen);
   };
 
+  const hadleDeletePicture = (index) => {
+    const newImages = [...images];
+    newImages.splice(index, 1);
+    setImages(newImages);
+  };
+
+  const handleUpdateDescription = (index, description) => {
+    const newImages = [...images];
+    newImages[index].description = description;
+    setImages(newImages);
+  };
+
+  const handleUpdatePicture = (index) => (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const newImages = [...images];
+      newImages[index] = {
+        ...newImages[index],
+        file: file,
+        url: URL.createObjectURL(file),
+      };
+      setImages(newImages);
+    }
+  };
   return (
     <>
       {/* <div className="grid grid-cols-3 gap-4">
@@ -107,7 +137,7 @@ function ImageGallery() {
                 <p className="pr-1">drag and drop your images or </p>
                 <label
                   htmlFor="file-upload"
-                  className="relative cursor-pointer rounded-md bg-white font-semibold text-dark focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2"
+                  className="relative cursor-pointer rounded-md bg-white font-semibold text-dark outline-none"
                 >
                   <span>
                     {" "}
@@ -127,21 +157,113 @@ function ImageGallery() {
           </div>
         )}
         {images.length > 0 && (
-        <div
-          onClick={handleEditPictures} 
-         class="flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10 cursor-pointer"
-        >
-          <div class="relative w-full h-[12rem] overflow-hidden flex position-relative">
-            {images.map((image, index) => (
-              <img key={index} className={`border border-black w-[20rem] h-[12rem] absolute`} style={{ left: `${index * 6}rem` }} src={image.url} alt={`Preview ${index}`} />
-            ))}
-            {/* <img className="border border-black w-[20rem] h-[12rem] absolute" src="https://picsum.photos/id/1/200/300" alt="" />
-            <img className="border border-black w-[20rem] h-[12rem] absolute left-[6rem]" src="https://fastly.picsum.photos/id/11/2500/1667.jpg?hmac=xxjFJtAPgshYkysU_aqx2sZir-kIOjNR9vx0te7GycQ" alt="" />
-            <img className="border border-black w-[20rem] h-[12rem] absolute left-[12rem]" src="https://fastly.picsum.photos/id/10/2500/1667.jpg?hmac=J04WWC_ebchx3WwzbM-Z4_KC_LeLBWr5LZMaAkWkF68" alt="" />
-            <img className="border border-black w-[20rem] h-[12rem] absolute left-[18rem]" src="https://fastly.picsum.photos/id/2/5000/3333.jpg?hmac=_KDkqQVttXw_nM-RyJfLImIbafFrqLsuGO5YuHqD-qQ" alt="" />
-            <img className="border border-black w-[20rem] h-[12rem] absolute left-[24rem]" src="https://fastly.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU" alt="" />   */}
+          <div
+            onClick={handleEditPictures}
+            className="flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10 cursor-pointer"
+          >
+            <div className="relative w-full h-[12rem] overflow-hidden flex position-relative">
+              {images.map((image, index) => (
+                <img
+                  key={index}
+                  className={`border border-black w-[20rem] h-[12rem] absolute bg-white`}
+                  style={{ left: `${index * 6}rem` }}
+                  src={image.url}
+                  alt={`Preview ${index}`}
+                />
+              ))}
+            </div>
           </div>
-        </div>
+        )}
+        {isOpen && (
+          <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-[99999]">
+            <div className="bg-white p-6 rounded-lg shadow-xl relative max-w-5xl">
+              <button
+                onClick={togglePopup}
+                className="absolute top-2 right-4 text-xl text-black rounded-full p-2 hover:text-black"
+              >
+                ✕
+              </button>
+              <h2 className="text-xl font-bold mb-4">Room Photos Gallery</h2>
+              <div className="grid grid-cols-3 gap-8">
+                {images.map((image, index) => (
+                  <div key={index} className="border border-[#a7a7a7]">
+                    <div className="border-b border-[#a7a7a7] p-2 group relative content-center h-[13rem] overflow-hidden">
+                      <img
+                        src={image.url}
+                        alt={`Preview ${index}`}
+                        className="max-h-[12rem] m-auto w-full h-full"
+                      />
+                      <span className="text-[#ffffff00] group-hover:text-black group-hover:bg-white rounded-md absolute bottom-4 left-4 cursor-pointer transition duration-300 ">
+                        <MdDeleteForever
+                          onClick={() => hadleDeletePicture(index)}
+                          className="w-8 h-8"
+                        />
+                      </span>
+
+                     {/*  <span className="text-[#ffffff00] group-hover:text-black group-hover:bg-white rounded-md absolute bottom-4 left-16 p-2 cursor-pointer transition duration-300 ">
+                        <input
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          onChange={handleUpdatePicture(index)}
+                        >
+                          <FaPen className="w-4 h-4" />
+                        </input>
+                      </span> */}
+
+                      <label
+                        htmlFor={`file-update-${index}`}
+                        className="text-[#ffffff00] group-hover:text-black group-hover:bg-white rounded-md absolute bottom-4 left-16 p-2 cursor-pointer transition duration-300"
+                      >
+                        <input
+                          id={`file-update-${index}`}
+                          type="file"
+                          className="hidden"
+                          onChange={handleUpdatePicture(index)}
+                        />
+                        <FaPen className="w-4 h-4" />
+                      </label>
+
+                    </div>
+                    <input
+                      className="w-full p-3 outline-none"
+                      type="text"
+                      name=""
+                      id=""
+                      value={image.description}
+                      onChange={(e) =>
+                        handleUpdateDescription(index, e.target.value)
+                      }
+                      placeholder="Add a description"
+                    />
+                  </div>
+                ))}
+                <div className="flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                  <div className="text-center content-center">
+                    <p className="mx-auto text-gray-300">
+                      <FaCloudUploadAlt className="m-auto w-14 h-14" />
+                    </p>
+                    <div className="mt-4 flex text-xl leading-6 text-black">
+                      <label
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer rounded-md bg-white font-semibold text-dark outline-none"
+                      >
+                        <span>Add more images</span>
+                        <input
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          className="sr-only outline-none"
+                          multiple
+                          onChange={handleFileChange}
+                        />
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         )}
       </div>
     </>
